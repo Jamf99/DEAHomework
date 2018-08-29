1d21d1499c9f4433822cf140050739d99864b1f3
Jamf99 <jamf_99@hotmail.com>
2018-08-29 09:32:33 -0500

    Hello!

--- ADEPractices/src/model1/Balancer.java
+++ ADEPractices/src/model1/Balancer.java
@@ -2,8 +2,8 @@ package model1;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
+//import java.io.File;
+//import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
